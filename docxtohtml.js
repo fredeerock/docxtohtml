9884c82f54aa5b8ac7f28c0319f5471dbc248082
Derick Ostrenko <dostrenko@gmail.com>
2015-01-04 18:01:32 -0600

    readme

--- docxtohtml.js
+++ docxtohtml.js
@@ -1,11 +1,24 @@
-// This script is meant to take a .docx downloaded from google docs and convert it to a nicely formatted .html file. It is assumed the google docs file is made with almost exclusively with the tags <ul> and <li>.
+/*This script is meant to take a .docx downloaded from google docs and convert it to a nicely formatted .html file. It is assumed the google docs file is made with almost exclusively with the tags <ul> and <li>.
 
-// # Dependencies
-// pandoc
-// node
+# Dependencies
+homebrew (for installing pandoc and node)
+pandoc
+node
 
-// # Usage
-// node replace.js <filename.docx>  
+# Installtion
+Using [Homebrew] (http://brew.sh/) you can instal node and pandoc using the following
+**1. Homebrew**
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+**2. Pandoc**    
+    brew install pandoc
+**3. Node**    
+    brew install node
+
+# Usage
+node replace.js <filename.docx>
+
+# Notes
+basically a mashup of http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback and http://javascript.cs.lmu.edu/notes/commandlinejs/*/
 
 var fs = require('fs');
 
