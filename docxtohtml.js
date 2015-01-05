d37bcb834ae39f0e7fc2ebdb3441fef8291aa0d2
Derick Ostrenko <dostrenko@gmail.com>
2015-01-04 18:16:10 -0600

    comments

--- docxtohtml.js
+++ docxtohtml.js
@@ -1,25 +1,30 @@
 /*
-This script is meant to take a .docx downloaded from google docs and convert it to a nicely formatted .html file. It is assumed the google docs file is made with almost exclusively with the tags <ul> and <li>.
+#About
+This script is meant to take a .docx downloaded from google docs and convert it to a nicely formatted .html file. It is assumed the google docs file is made with almost exclusively with the tags `<ul>` and `<li>`.
 
 # Dependencies
-homebrew (for installing pandoc and node)
-pandoc
-node
+- homebrew (for installing pandoc and node)
+- pandoc
+- node
 
-# Installtion
-Using [Homebrew] (http://brew.sh/) you can instal node and pandoc using the following
-**1. Homebrew**
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Installation
+Using [Homebrew] (http://brew.sh/) you can instal node and pandoc using the following.    
+
+**1. Homebrew**    
+
+`ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`    
 **2. Pandoc**    
-    brew install pandoc
+`brew install pandoc`    
 **3. Node**    
-    brew install node
+`brew install node`
 
 # Usage
-    node replace.js <filename.docx>
+    node docxtohtml.js <filename.docx>
 
 # Notes
-basically a mashup of http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback and http://javascript.cs.lmu.edu/notes/commandlinejs/
+This is basically a mashup of the following.
+- http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+- http://javascript.cs.lmu.edu/notes/commandlinejs/
 */
 
 var fs = require('fs');
